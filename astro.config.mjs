987b963679536d8479fade72425c4c9b52a59bb3
Drej57 <164466759+Drej57@users.noreply.github.com>
2025-05-17 00:47:04 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // Replace 'your-username' with your actual GitHub username
-const GITHUB_USERNAME = 'your-username';
+const GITHUB_USERNAME = 'Drej57';
 const REPO_NAME = 'verdant-virgo';
 
 // https://astro.build/config
 export default defineConfig({
 	site: `https://${GITHUB_USERNAME}.github.io`,
-	// base: `/${REPO_NAME}`,
+	base: `/${REPO_NAME}`,
 	integrations: [mdx(), sitemap()],
 });
